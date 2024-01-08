570f2b8ca093418f32116aab0dc2d39ad2234e15
刘智慧 <zhliu@luopan88.com>
2024-01-08 13:27:02 +0800

    feature:导航栏

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,15 @@
-<template>首页</template>
+<template>
+  <nav-bar></nav-bar>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/nav-bar/index.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
