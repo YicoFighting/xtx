3701db6a239ab861e0cfadefe1d9f946dc810333
刘智慧 <zhliu@luopan88.com>
2024-01-10 15:09:12 +0800

    feature:用户模块

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -71,7 +71,7 @@ export const http = <T>(options: UniApp.RequestOptions) => {
           // 401错误  -> 清理用户信息，跳转到登录页
           const memberStore = useMemberStore()
           memberStore.clearProfile()
-          uni.navigateTo({ url: '/pages/login/login' })
+          uni.navigateTo({ url: '/pages/login/index' })
           reject(res)
         } else {
           // 其他错误 -> 根据后端错误信息轻提示
